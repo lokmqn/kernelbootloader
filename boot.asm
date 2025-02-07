74d95551878faa86d01723fd326ea96ff2c7abeb
polarstar 𒉭 <137574070+lokmqn@users.noreply.github.com>
2025-02-08 03:01:33 +0900

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,30 +1,28 @@
-ORG 0x7c00
-BITS 16 
+ORG 0x7C00
+BITS 16
 
 start:
-
-  move si, message
-
+  mov si, message
   call print
-  
-    jmp $
+  jmp $
+
 print:
   mov bx, 0
-.loop:  
+.loop:
   lodsb
   cmp al, 0
   je .done
   call print_char
   jmp .loop
 .done:
-  ret  
-  print_char:   
-   mov ah, 0eh
-   int 0x10 ; calling the bios .
-   ret
-   message: db 'Hello Polar',0
+  ret
 
- times 510- ($ -$$) db 0 ;to fill at least 510 bytes of data but if we dont it will pad the rest with 0
- dw 0xAA55 ;fot intel machines and dw will put 0xAA55 as binary
+print_char:
+  mov ah, 0x0E
+  int 0x10
+  ret
 
+message: db 'Hello Polar', 0
 
+times 510- ($ -$$) db 0
+dw 0xAA55
